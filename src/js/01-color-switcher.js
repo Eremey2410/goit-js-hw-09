b45f02e0bd89beb8f69c305b59332a056b66bb4c
Eremey2410 <2410esv@gmail.com>
2022-08-12 22:00:10 +0300

    add 9-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+};
 
+let timerId = null;
+
+refs.start.addEventListener('click', () => {
+  refs.start.disabled = true;
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+refs.stop.addEventListener('click', () => {
+  refs.start.disabled = false;
+  clearInterval(timerId);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
