4a77520008ad1933dafdda6237607f38562a55a8
Eremey2410 <97343744+Eremey2410@users.noreply.github.com>
2022-08-12 19:00:48 +0000

    Deploying to gh-pages from @ Eremey2410/goit-js-hw-09@b45f02e0bd89beb8f69c305b59332a056b66bb4c 🚀

---
+++ 01-color-switcher.1f150fdf.js
@@ -0,0 +1,2 @@
+!function(){var t={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]")},n=null;t.start.addEventListener("click",(function(){t.start.disabled=!0,n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),t.stop.addEventListener("click",(function(){t.start.disabled=!1,clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.1f150fdf.js.map
---
+++ 01-color-switcher.81630aaf.js
@@ -0,0 +1,2 @@
+const t={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]")};let e=null;t.start.addEventListener("click",(()=>{t.start.disabled=!0,e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.stop.addEventListener("click",(()=>{t.start.disabled=!1,clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.81630aaf.js.map
